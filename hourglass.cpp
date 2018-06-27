4c9195ef30bd404b5810e2c33a46f75d1c382522
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2018-06-28 00:12:02 +0545

    Add files via upload

---
+++ hourglass.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int ar[6][6] , maxm = -99999;
+
+    for(int i = 0; i < 6; i++){
+      for(int j = 0; j < 6; j++){
+	  cin >> ar[i][j];
+      }
+    }
+    int sum = 0;
+    for(int i = 1; i < 5; i++){
+        for(int j = 1; j < 5; j++){
+            sum = ar[i][j]+ar[i-1][j]+ar[i-1][j-1]+ar[i-1][j+1]+ar[i+1][j]+ar[i+1][j-1]+ar[i+1][j+1];
+            if(maxm < sum){
+	      maxm = sum;
+	    }
+        }
+    }
+    cout << maxm << endl;
+    return 0;
+}
